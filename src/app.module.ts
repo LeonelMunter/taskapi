cb272ac0860621ed7510d3cfc478d70564d9d561
leonelmunter <leonelmunter@gmail.com>
2023-11-21 14:22:15 -0300

    deploy setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config/dist';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'), TasksModule]
+    MongooseModule.forRoot('mongodb://localhost/nest'), TasksModule, ConfigModule.forRoot()]
     
 })
 export class AppModule {}
